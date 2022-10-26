b5eed530473880d7a44766a0c4124af5fddcf3b8
arhanz <arhaanzeeshan13@gmail.com>
2022-10-26 09:47:27 +0500

    Create Assigment1.js

---
+++ assingment1.js
@@ -0,0 +1,8 @@
+const lonely = [4, 7, 9, 3, 9, 7, 4];
+let findSingleValue = lonely.filter((lonelyValue) => {
+  return (
+    lonely.findIndex((UniqueValue) => UniqueValue == lonelyValue) ==
+    lonely.lastIndexOf(lonelyValue)
+  );
+});
+console.log("The Single Value is " + String(findSingleValue));
